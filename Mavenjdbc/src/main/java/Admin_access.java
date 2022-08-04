82957e7ee04087cac9e8f447f13c172079f1cbb1
Rupan2324 <108411408+Rupan2324@users.noreply.github.com>
2022-08-04 22:09:36 +0530

    Updated the code with insert method

--- Mavenjdbc/src/main/java/Admin_access.java
+++ Mavenjdbc/src/main/java/Admin_access.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Formatter;
 import java.util.Scanner;
@@ -13,27 +14,88 @@ public class Admin_access {
     	ad.displayServices();
 	System.out.println("*****************************");
 	System.out.println("*                           *");
-	System.out.println("Press 1 to update any details");
-	System.out.println("Press 2 to delete any details");
-	System.out.println("Press 3 to view any details");
+	System.out.println("Press 1 to insert any details");
+	System.out.println("Press 2 to update any details");
+	System.out.println("Press 3 to delete any details");
+	System.out.println("Press 4 to view any details");
 	System.out.println("*                           *");
 	System.out.println("*****************************");
 	int i = in.nextInt();
 
 	switch(i) {
 	case 1:
+		ad.insert();
+	case 2:
 		ad.update();
 		break;
-	case 2:
+	case 3:
 		ad.delete();
 		break;
-	case 3:
+	case 4:
 		ad.select();
 		break;
 	default:
 		System.err.println("Wrong Choice");
 		break;
 	}
+	}
+	public void insert() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("*****Please enter User details*****");
+		try {
+			String m = "";
+	      Book bk = new Book();
+		 Connection con = bk.getconn();
+		 String s1 = "insert into services(name,address,Type_of_service,booking_date,phone_no) values(?,?,?,?,?)";
+		 PreparedStatement stmt = con.prepareStatement(s1);
+		             System.out.println("Enter user's Name: ");
+		             String name = in.nextLine();
+		             System.out.println("Enter user's address: ");
+		             String address = in.nextLine();
+		             System.out.println("Enter the type of service they want");
+		             String Type_of_service = in.nextLine();
+		             if(Type_of_service.equalsIgnoreCase("CAR WASHING") || 
+		            		 Type_of_service.equalsIgnoreCase("ELECTRICIAN") ||
+		            		 Type_of_service.equalsIgnoreCase("TV REPAIR") ||
+		            		 Type_of_service.equalsIgnoreCase("PLUMBER")){
+		             System.out.println("Enter the booking date in the format of 'YYYY-MM-DD': ");
+		             String date = in.nextLine();
+		        
+		             System.out.println("Enter their 10 digit phone number: ");
+		             String phone_no = in.nextLine();
+		             if(phone_no.length()==10) {
+		       stmt.setString(1,name);
+		       stmt.setString(2,address);			     
+		       stmt.setString(3, Type_of_service);
+		       stmt.setString(4,date);
+		       stmt.setString(5,phone_no);
+		       System.out.println("A row is successfully inserted");
+		       int insert = stmt.executeUpdate();
+		             }
+		             else {
+		            	 System.err.println("Sorry..Phone number should be in 10 digits..");
+		    
+		             }
+		             }
+		             else {
+				    	   System.err.println("Sorry this service is not provided here...");
+				       }
+		             String r = "";
+		     		System.out.println("If you want to continue again then press y");
+		     		r=in.next();
+		             if(r.equals("y")) {
+		             	Admin_access ad = new Admin_access();
+		             	ad.admin();
+		             }
+		             else {
+		             	System.out.println("************Session Closed...Thank You Visit Again...*************");
+		     			System.exit(0);
+		             }
+		}
+		catch(SQLException esql) {
+			System.err.println("Fill all the required fields... "+esql);
+		}
+		   
 	}
 	
 	public void update() {
